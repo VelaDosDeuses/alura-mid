0876cc16be37370590f52baf8bf698072609ac08
VelaDosDeuses <114483311+VelaDosDeuses@users.noreply.github.com>
2023-09-25 11:43:57 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-function TocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function TocaSomjonathan () {
+    document.querySelector('#som_tecla_jonathan').play();
 }
 
-document.querySelector('.tecla_pom');
+document.querySelector('.tecla_jonathan').onclick = TocaSomjonathan;
 
