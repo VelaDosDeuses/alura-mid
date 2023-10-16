caf5668ad13175c7ef822f64ff2dd990ef7343d7
VelaDosDeuses <114483311+VelaDosDeuses@users.noreply.github.com>
2023-10-16 11:25:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,16 +4,20 @@ function tocaSom (idElementAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla')
 
-let contador = 0;
-
 //enquanto
-while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = function () {
-        tocaSom('#som_tecla_pom');
-    }
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-    contador = contador + 1;
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    
+    const idAudio = `#som_${instrumento}`;//template string
+
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
 
     console.log(contador);
 
 }
+    
